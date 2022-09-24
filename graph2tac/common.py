d910220e1deeaaf6b1a00bc69b46376e18bcab2d
Jason Rute <Jason.Rute@ibm.com>
2022-09-24 16:38:15 -0400

    Improve logging
    Signed-off-by: Jason Rute <Jason.Rute@ibm.com>
    Signed-off-by: Mirek Olsak <mirek@olsak.net>

--- graph2tac/common.py
+++ graph2tac/common.py
@@ -55,5 +55,11 @@ logging.basicConfig(
 )
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger('graph2tac')
+
+logging.addLevelName(15, "VERBOSE")  # between debug and info
 logger.verbose = lambda message: logger.log(level=15, msg=message)
+
+logging.addLevelName(25, "SUMMARY")  # between info and warning
+logger.summary = lambda message: logger.log(level=25, msg=message)
+
 logger.setLevel(logging.INFO)
