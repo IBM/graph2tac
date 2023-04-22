84e89716d4d5c51982ea67549cf475dc401ec38b
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-04-22 18:06:06 +0200

    Add dataset with duplicated definitions
    Signed-off-by: Lasse Blaauwbroek <lasse@blaauwbroek.eu>

---
+++ tests/data/duplicate_identities/theories/Duplicates.v
@@ -0,0 +1,44 @@
+From Tactician Require Export Ltac1.Record.
+From Tactician Require Export Ltac1.
+Set Tactician Neural Executable "g2t-server --arch tfgnn --log_level=info --tf_log_level=critical --tactic_expand_bound=3 --total_expand_bound=10 --search_expand_bound=4 --model MODEL --record record_file.bin".
+Global Set Default Proof Mode "Tactician Ltac1".
+
+Reserved Notation "x -> y" (at level 99, right associativity, y at level 200).
+Notation "A -> B" := (forall (_ : A), B).
+
+Goal Prop -> Prop.
+intro.
+assumption.
+Qed.
+
+Section X.
+Variable A : Prop.
+Definition t : A -> A.
+try (synth) || (intro; assumption).
+Qed.
+End X.
+
+Section X.
+Variable A : Prop. (* Duplicate! *)
+Definition u : A -> A.
+try (synth) || (intro; assumption).
+Qed.
+End X.
+
+Module Type Y.
+Parameter B : Prop.
+End Y.
+
+Module M1(MyY : Y).
+Definition t : MyY.B -> MyY.B.
+try (synth) || (intro; assumption).
+Qed.
+End M1.
+
+Definition somethinguseless := Prop.
+
+Module M2(MyY : Y). (* Duplicate MyY.B *)
+Definition t : MyY.B -> MyY.B.
+try (synth) || (intro; assumption).
+Qed.
+End M2.
