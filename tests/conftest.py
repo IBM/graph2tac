320553835974c972ece5094d3c1d0ce0c36fde10
Jason Rute <Jason.Rute@ibm.com>
2023-04-24 14:09:16 -0400

    Refactor tests to cache results
    Signed-off-by: Jason Rute <Jason.Rute@ibm.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,12 @@ import pytest
 # add an option to pytest to overwrite expected data for a test
 def pytest_addoption(parser):
     parser.addoption("--overwrite", action="store_true", default=False, help="Overwrite test data with new results")
+    parser.addoption("--retrain", action="store_true", default=False, help="Retrain saved models")
 
 @pytest.fixture(scope='session')
 def overwrite(request):
     return request.config.getoption("--overwrite")
+
+@pytest.fixture(scope='session')
+def retrain(request):
+    return request.config.getoption("--retrain")
