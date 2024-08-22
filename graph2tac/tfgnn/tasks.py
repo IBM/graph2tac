f084980e6b2405fe130fb20b14bf65be6be7a64e
Jason Rute <Jason.Rute@ibm.com>
2024-08-22 07:04:36 -0400

    Hardcode temp to 0.008
    Signed-off-by: Jason Rute <Jason.Rute@ibm.com>

--- graph2tac/tfgnn/tasks.py
+++ graph2tac/tfgnn/tasks.py
@@ -1307,7 +1307,7 @@ class QueryKeyMulGlobal(tf.keras.layers.Layer):
             # so logits can be in a wider or narrower range -1/temp to 1/temp
 
             assert temp is not None
-            self._temp = temp
+            self._temp = tf.constant(0.008) # hard coded for testing old stdlib model because of bug during training
         self.query_key_mul = QueryKeyMul()
 
     def unit_normalize_tensor(self, x: tf.Tensor) ->  tf.Tensor:
